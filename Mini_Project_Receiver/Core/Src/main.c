d08d38c25bf152fa8f3821b7ef80245163e97b14
OmolemoKegakilwe <110494747+OmolemoKegakilwe@users.noreply.github.com>
2022-10-20 18:50:29 +0200

    Transmitter side: senddata v0, Receiver side: project setup

--- Mini_Project_Receiver/Core/Src/main.c
+++ Mini_Project_Receiver/Core/Src/main.c
@@ -43,9 +43,6 @@ with baud rate of 9600.
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-ADC_HandleTypeDef hadc;
-DMA_HandleTypeDef hdma_adc;
-
 TIM_HandleTypeDef htim3;
 
 UART_HandleTypeDef huart2;
@@ -55,7 +52,7 @@ DMA_HandleTypeDef hdma_usart2_tx;
 char buffer[10];
 int delay = 1000;
 int bit_duration = 100;
-int samples_sent;
+int samples_received;
 
 //TO DO:
 //TASK 1
@@ -68,14 +65,12 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_DMA_Init(void);
 static void MX_USART2_UART_Init(void);
-static void MX_ADC_Init(void);
 static void MX_TIM3_Init(void);
 /* USER CODE BEGIN PFP */
 void EXTI0_1_IRQHandler(void);
-uint16_t pollADC(void);
 uint16_t ADCtoCRR(uint16_t adc_val);
-void sendData(uint16_t adc_val);
-void sendCheckpoint(uint16_t samples);
+void receiveData();
+void receiveCheckpoint();
 
 /* USER CODE END PFP */
 
@@ -114,18 +109,19 @@ int main(void)
   MX_GPIO_Init();
   MX_DMA_Init();
   MX_USART2_UART_Init();
-  MX_ADC_Init();
   MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
 
   // For UART Transmit
   char buffer[20];
-  samples_sent=0;
+  samples_received=0;
   //Start the PWM on TIM3 Channel 4 (Green LED)
   HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_4);
 
   //GPIO Pin state
   GPIO_PinState state;
+
+
   uint16_t adc_val;
   /* USER CODE END 2 */
 
@@ -136,14 +132,15 @@ int main(void)
 	  state = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0);
 	  if (state)
 	  {
-		  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_SET);
-		  adc_val = pollADC();
-		  sendData(adc_val);
-		  samples_sent+=1;
+
+
+		  //start condition
+		  	  receiveData();
+		  	  samples_received+=1;
 	  }
 	  else
 	  {
-		  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_RESET);
+		  //continue listening
 	  }
 
 	  //Test your pollADC function and display via UART
@@ -157,7 +154,6 @@ int main(void)
 
 	  //Complete rest of implementation
 
-      //sendData(val)
 
 	  HAL_Delay (delay); // wait for 500 ms
 
@@ -180,11 +176,9 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_HSI14;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSI14State = RCC_HSI14_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.HSI14CalibrationValue = 16;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
   RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL12;
@@ -206,60 +200,6 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-}
-
-/**
-  * @brief ADC Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_ADC_Init(void)
-{
-
-  /* USER CODE BEGIN ADC_Init 0 */
-
-  /* USER CODE END ADC_Init 0 */
-
-  ADC_ChannelConfTypeDef sConfig = {0};
-
-  /* USER CODE BEGIN ADC_Init 1 */
-
-  /* USER CODE END ADC_Init 1 */
-
-  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
-  */
-  hadc.Instance = ADC1;
-  hadc.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
-  hadc.Init.Resolution = ADC_RESOLUTION_12B;
-  hadc.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-  hadc.Init.ScanConvMode = ADC_SCAN_DIRECTION_FORWARD;
-  hadc.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
-  hadc.Init.LowPowerAutoWait = DISABLE;
-  hadc.Init.LowPowerAutoPowerOff = DISABLE;
-  hadc.Init.ContinuousConvMode = DISABLE;
-  hadc.Init.DiscontinuousConvMode = DISABLE;
-  hadc.Init.ExternalTrigConv = ADC_SOFTWARE_START;
-  hadc.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
-  hadc.Init.DMAContinuousRequests = DISABLE;
-  hadc.Init.Overrun = ADC_OVR_DATA_PRESERVED;
-  if (HAL_ADC_Init(&hadc) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Configure for the selected ADC regular channel to be converted.
-  */
-  sConfig.Channel = ADC_CHANNEL_7;
-  sConfig.Rank = ADC_RANK_CHANNEL_NUMBER;
-  sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
-  if (HAL_ADC_ConfigChannel(&hadc, &sConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN ADC_Init 2 */
-
-  /* USER CODE END ADC_Init 2 */
-
 }
 
 /**
@@ -366,9 +306,6 @@ static void MX_DMA_Init(void)
   __HAL_RCC_DMA1_CLK_ENABLE();
 
   /* DMA interrupt init */
-  /* DMA1_Channel1_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(DMA1_Channel1_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(DMA1_Channel1_IRQn);
   /* DMA1_Channel4_5_IRQn interrupt configuration */
   HAL_NVIC_SetPriority(DMA1_Channel4_5_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(DMA1_Channel4_5_IRQn);
@@ -388,13 +325,9 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOF_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : PA0 */
   GPIO_InitStruct.Pin = GPIO_PIN_0;
@@ -409,33 +342,12 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(LD4_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : PB6 */
-  GPIO_InitStruct.Pin = GPIO_PIN_6;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
 }
 
 /* USER CODE BEGIN 4 */
 
 
 
-uint16_t pollADC(void){     // get ADC value
-	//TO DO:
-	//TASK 2
-	// Complete the function body
-    uint32_t timeout = 100;//in milliseconds //timeout = 500ms?
-    HAL_ADC_Start(&hadc);
-    HAL_ADC_PollForConversion(&hadc, 100);
-
-    uint32_t val = HAL_ADC_GetValue(&hadc);
-    HAL_ADC_Stop(&hadc);
-    
-    //ADC has 12-bit resolution
-	return val;
-}
 
 uint16_t ADCtoCRR(uint16_t adc_val){    // convert ADC value to PWM duty cycle (CRR) value
 	//TO DO:
@@ -448,33 +360,16 @@ uint16_t ADCtoCRR(uint16_t adc_val){    // convert ADC value to PWM duty cycle (
     //if the value is 200, then green LED will be on for 200 cycles and of for 3896 cycles
     uint32_t ccr_val = adc_val* 47999/4095 ;//change to suitable value
     uint32_t arr_val = 47999;
-    HAL_TIM_PWM_Start(&hadc, TIM_CHANNEL_4);
-    HAL_ADC_Start_IT(&hadc);
-    uint32_t val = 100*ccr_val/arr_val;
-    __HAL_TIM_SetCompare(&htim3, TIM_CHANNEL_4, ccr_val);
-    HAL_ADC_Stop_IT(&hadc);
 
+    uint32_t val = 100*ccr_val/arr_val;
 
 	return val;
 }
 
-void sendData(uint16_t data){
-	GPIO_PinState state;
-	for (int i = 16; i>0 ; i--)
-	{
-        if((data & 0x0001)==1)
-        {
-        	state=GPIO_PIN_SET;
-        }
-        else state=GPIO_PIN_RESET;
+void receiveData(){
 
-        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, state);
-        HAL_Delay(bit_duration);
-
-        data >>= 1;
-	}
 }
-void sendCheckpoint(uint16_t samples)
+void receiveCheckpoint()
 {
 
 }
